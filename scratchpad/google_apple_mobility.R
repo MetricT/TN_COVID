4008dbb3a1b2b48439d47ace43de2a67fa6adbc9
MetricT <MetricT@like.naw.dawg>
2020-08-04 10:08:49 -0500

    * Fiddling with the Google/Apple mobility data.   Just commiting so I can sync to work.

--- scratchpad/google_apple_mobility.R
+++ scratchpad/google_apple_mobility.R
@@ -38,7 +38,7 @@ apple_model <-
   components()
 
 g_apple <-
-  ggplot(data = apple_cheatham, aes(x = as.Date(dates))) +
+  ggplot(data = apple_county, aes(x = as.Date(dates))) +
   theme_linedraw() +
   geom_point(aes(y = values), alpha = 0.5) +
   geom_line(data = apple_model, aes(y = trend), size = 1, color = "firebrick3")
@@ -79,7 +79,7 @@ g_google <-
   ggplot(data = google_tn, aes(x = as.Date(date))) +
   theme_linedraw() +
   geom_line(aes(y = value), color = "darkseagreen4", size = 1.0) +
-  labs(title = "% Change from baseline")
+  labs(title = "% Change from baseline") +
   facet_wrap(~ name, scales = "free_y")
 
 print(g_google)
@@ -98,6 +98,7 @@ g_tn <-
   google_tn %>% 
   as_tibble() %>%
   filter(name == "workplaces") %>% 
+  mutate(date = as.Date(date) - 8) %>%
   mutate(name = "Google change in visits to workplace in TN,\n% change from baseline") %>%
   bind_rows(tn_unemploy)
 
@@ -106,7 +107,58 @@ g_google_unemploy <-
   theme_linedraw() +
   theme(strip.text = element_text(size = 20)) + 
   geom_line(aes(y = value), color = "darkseagreen4", size = 1.0) +
-  geom_vline(xintercept = as.Date("2020-06-01") + 15, linetype = "dotted") +
+  geom_vline(xintercept = as.Date("2020-02-01"), linetype = "dotted") +
+  geom_vline(xintercept = as.Date("2020-03-01"), linetype = "dotted") +
+  geom_vline(xintercept = as.Date("2020-04-01"), linetype = "dotted") +
+  geom_vline(xintercept = as.Date("2020-05-01"), linetype = "dotted") +
+  geom_vline(xintercept = as.Date("2020-06-01"), linetype = "dotted") +
+  geom_vline(xintercept = as.Date("2020-07-01"), linetype = "dotted") +
   facet_wrap(~ name, scales = "free_y")
 print(g_google_unemploy)
 
+
+
+
+google_us <-
+  google_mobility %>% 
+  mutate(date = as.Date(date)) %>%
+  filter(country_region_code == "US") %>%
+  select(date, workplaces_percent_change_from_baseline) %>%
+  group_by(date) %>%
+  summarize(workplaces_values = sum(workplaces_percent_change_from_baseline, na.rm = TRUE)) %>%
+  ungroup() %>%
+  as_tsibble(index = date) %>%
+  tsibble::fill_gaps() %>% 
+  na.locf() %>% 
+  mutate(workplaces = workplaces_values %>% ts() %>% mstl() %>% trendcycle()) %>% 
+  select(-workplaces_values) %>%
+  pivot_longer(-date)
+
+unemploy_us <-
+  fredr(series_id = "UNRATE",
+        frequency = "m",
+        observation_start = as.Date("2020-02-01")) %>%
+  rename(name = series_id) %>%
+  mutate(name = "0 - US Unemployment [UNRATE]") %>%
+  mutate(value = - value)
+
+g_us <-
+  google_us %>% 
+  as_tibble() %>%
+  mutate(date = as.Date(date) - 8) %>%
+  mutate(name = "Google change in visits to workplace in US,\n% change from baseline") %>%
+  bind_rows(unemploy_us)
+
+g_google_unemploy_us <-
+  ggplot(data = g_us, aes(x = as.Date(date))) +
+  theme_linedraw() +
+  theme(strip.text = element_text(size = 20)) + 
+  geom_line(aes(y = value), color = "darkseagreen4", size = 1.0) +
+  geom_vline(xintercept = as.Date("2020-02-01"), linetype = "dotted") +
+  geom_vline(xintercept = as.Date("2020-03-01"), linetype = "dotted") +
+  geom_vline(xintercept = as.Date("2020-04-01"), linetype = "dotted") +
+  geom_vline(xintercept = as.Date("2020-05-01"), linetype = "dotted") +
+  geom_vline(xintercept = as.Date("2020-06-01"), linetype = "dotted") +
+  geom_vline(xintercept = as.Date("2020-07-01"), linetype = "dotted") +
+  facet_wrap(~ name, scales = "free_y")
+print(g_google_unemploy_us)
